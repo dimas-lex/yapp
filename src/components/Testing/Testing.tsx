99d015b16d3525ba46a852515c6cd743bb497bf5
dimas <dimaslex@gmail.com>
2021-01-17 22:05:04 +0100

    Add statistic

--- src/components/Testing/Testing.tsx
+++ src/components/Testing/Testing.tsx
@@ -1,39 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import list from './list.json';
 import './Testing.css';
-
-function getRandomArbitrary(max: number, min = 0) {
-  return  Math.round(Math.random() * (max - min) + min);
-}
-
+ 
 export const Testing = () => {
   const [activeIndex, setIndex] = useState(1);
+  const [statistic, setStatistic] = useState({} as { [key: number]: number });
   const [answer, setAnswer] = useState('');
-  const [status, setStatus] = useState(getRandomArbitrary(list.length));
+  const [status, setStatus] = useState(0);
   const [formClasses, setFormClasses] = useState('');
   const [answerClasses, setAnswerClasses] = useState('');
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.stopPropagation();
-    event.preventDefault();
+  const getRandomArbitrary = useCallback((max: number, min = 0, level = 0): number => { 
+    const newIndex = Math.round(Math.random() * (max - min) + min);
+    console.log(`newIndex=${newIndex} statistic[newIndex] = ${statistic[newIndex]}`, statistic)
+    if (statistic[newIndex] > 5 && level < 20) {
+      console.log('try new', level)
+      return getRandomArbitrary(max, min, level + 1);
+    }
+  
+    return newIndex;
+  },[statistic]);
 
+
+  const onSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.stopPropagation();
+    event.preventDefault(); 
     const correctAnswer = list[activeIndex]?.answer;
 
     if (String(answer).toLocaleLowerCase() === String(correctAnswer).toLocaleLowerCase()) {
-      console.log(list.length);
-      const newIndex = getRandomArbitrary(list.length);
+      setStatistic({
+        ...statistic,
+        [activeIndex]: (statistic[activeIndex] ? statistic[activeIndex] : 0) + 1,
+      });
 
-      console.log(`newIndex ${newIndex}`);
+      const newIndex = getRandomArbitrary(list.length - 1);
 
       setIndex(newIndex);
       setStatus(1);
     } else {
-      setStatus(-1);
+      setStatus(-1);   setStatistic({
+        ...statistic,
+        [activeIndex]: (statistic[activeIndex] ? statistic[activeIndex] : 0) - 1,
+      });
     }
     setAnswer('');
-  }
+  }, [list, activeIndex, answer, getRandomArbitrary, statistic])
 
-  useEffect(() => { 
+  useEffect(() => {
     if (status === 1) setFormClasses('form--success');
     if (status === -1) {
       setFormClasses('form-error');
